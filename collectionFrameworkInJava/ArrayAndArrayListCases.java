8d957f53cb01396df250a6228dab3a584d6b6ea0
majumdar.anwesha <anwesha.majumdar@gmail.com>
2025-07-20 17:43:21 +0530

    test commit

--- collectionFrameworkInJava/ArrayAndArrayListCases.java
+++ collectionFrameworkInJava/ArrayAndArrayListCases.java
@@ -1,23 +1,30 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-class ArrayAndArrayListCase{
-public static void main(String[] args) {
-    ArrayListOnly ao=new ArrayListOnly();
-    ao.printArray();
+class ArrayAndArrayListCase {
+
+    public static void main(String[] args) {
+        ArrayListOnly ao = new ArrayListOnly();
+        ao.printArray();
+    }
 }
-}
 
- class ArrayListOnly{
-    public void printArray(){
-        List<String> employeeList=new ArrayList<>();
-employeeList.add("emp1");
-employeeList.add("emp1");
-employeeList.add("emp1");
-employeeList.add("emp1");
+class ArrayListOnly {
 
-System.out.println(employeeList);
+    public void printArray() {
+        List<Integer> employeeList = new ArrayList<>();
+        employeeList.add(2);
+        employeeList.add(90);
+        employeeList.add(200);
+        employeeList.add(90);
 
-            }
+        System.out.println(employeeList);
+
+        int[] nums={2,4,6,77,88,40};
+        
+        System.out.println(Arrays.toString(nums));
+
+    }
 }
