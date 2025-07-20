6b69e7f03c7d857cb5f0170f532335237d70eb62
majumdar.anwesha <anwesha.majumdar@gmail.com>
2025-07-20 11:42:49 +0530

    test commit

--- collectionFrameworkInJava/ArrayAndArrayListCases.java
+++ collectionFrameworkInJava/ArrayAndArrayListCases.java
@@ -1,5 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 class ArrayAndArrayListCase{
 public static void main(String[] args) {
-    System.out.println("hello");
+    ArrayListOnly ao=new ArrayListOnly();
+    ao.printArray();
 }
+}
+
+ class ArrayListOnly{
+    public void printArray(){
+        List<String> employeeList=new ArrayList<>();
+employeeList.add("emp1");
+employeeList.add("emp1");
+employeeList.add("emp1");
+employeeList.add("emp1");
+
+System.out.println(employeeList);
+
+            }
 }
