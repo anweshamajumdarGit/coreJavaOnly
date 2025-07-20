8cd3f5ce18f56cd4320e6110aaacbf29bf64caa4
majumdar.anwesha <anwesha.majumdar@gmail.com>
2025-07-20 18:08:17 +0530

    test commit

--- collectionFrameworkInJava/ArrayAndArrayListCases.java
+++ collectionFrameworkInJava/ArrayAndArrayListCases.java
@@ -8,6 +8,8 @@ class ArrayAndArrayListCase {
     public static void main(String[] args) {
         ArrayListOnly ao = new ArrayListOnly();
         ao.printArray();
+        WorkOnArrayList wo = new WorkOnArrayList();
+        wo.workOnPlace();
     }
 }
 
@@ -22,9 +24,26 @@ class ArrayListOnly {
 
         System.out.println(employeeList);
 
-        int[] nums={2,4,6,77,88,40};
-        
+        int[] nums = {2, 4, 6, 77, 88, 40};
+
         System.out.println(Arrays.toString(nums));
 
+    }
+}
+
+class WorkOnArrayList {
+
+    public void workOnPlace() {
+        List<String> places = new ArrayList<>();
+        places.add("Kolkata");
+        places.add("Mumbai");
+        places.add("Delhi");
+        places.add("Kerala");
+        for (String place : places) {
+            if (place.contains("K")) {
+                System.out.println(place);
+            }
+        }
+        System.out.println(places);
     }
 }
